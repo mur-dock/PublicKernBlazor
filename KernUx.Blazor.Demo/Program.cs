80522321b287efdd390093221e7ab9f0b2fa5ebb
Marcus Dock <Marcus.Dock@live.de>
2026-03-08 07:18:53 +0100

    feat(Showcase): restructure MainLayout and integrate KernUx components for improved showcase experience

--- KernUx.Blazor.Demo/Program.cs
+++ KernUx.Blazor.Demo/Program.cs
@@ -1,10 +1,12 @@
 using KernUx.Blazor.Demo.Components;
+using KernUx.Blazor.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddKernUx();
 
 var app = builder.Build();
 
