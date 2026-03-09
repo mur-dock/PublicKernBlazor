59226041bd2935c8960450c9734c332c52cd9c58
Marcus Dock <69457263+mur-dock@users.noreply.github.com>
2026-03-09 14:38:52 +0100

    feat: enhance layout structure and update styles for KernUxExample.Blazor

---
+++ KernUxExample.Blazor/Components/Pages/Weather.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KernUxExample.Blazor.Components.Pages;
+
+public class WeatherBase : ComponentBase
+{
+    protected WeatherForecast[]? _forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await Task.Delay(500);
+
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+        _forecasts = Enumerable.Range(1, 5)
+            .Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    protected sealed class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
